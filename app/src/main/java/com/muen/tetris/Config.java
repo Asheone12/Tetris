68daef04c4c89f9e516690654d547e8305d4083b
xujianglong <jianglong.xu@tiho-mobi.com>
2024-03-28 19:23:52 +0800

    首次提交，实现基本功能

---
+++ app/src/main/java/com/muen/tetris/Config.java
@@ -0,0 +1,13 @@
+package com.muen.tetris;
+
+public class Config {
+    //地图x方向格子数
+    public static final int MAPX = 10;
+    //地图u方向格子数
+    public static final int MAPY = 20;
+
+    //地图x方向宽度
+    public static int xWidth;
+    //地图y方向宽度
+    public static int yHeight;
+}
